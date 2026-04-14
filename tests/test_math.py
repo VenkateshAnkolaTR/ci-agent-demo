a56cf957e4ace24b8338277c0f263aeffdf7474b
Venkatesh <Venkatesh.Ankola@thomsonreuters.com>
2026-04-14 13:41:40 +0530

    Test failed

--- tests/test_math.py
+++ tests/test_math.py
@@ -1,7 +1,7 @@
 from src.math_utils import add, subtract, multiply
 
 def test_add():
-    assert add(2, 3) == 5
+    assert add(2, 3) == 500
 
 def test_subtract():
     assert subtract(10, 4) == 6
